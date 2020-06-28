6162bc90cdae9557bd8d9c03d26fd81bd1249829
Jack Holdsworth <jackch90@gmail.com>
2020-06-28 09:12:50 +0100

    Client Vue init

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('../public'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port);
